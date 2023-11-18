8c31de9fa5d87976050a98b2652bcca4b837457e
Venkateswarlu Kasireddy <85172370+venkateswarlu589@users.noreply.github.com>
2023-11-18 12:28:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum element in a sorted and rotated array/minimum-element-in-a-sorted-and-rotated-array.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+
+class Solution:
+    def findMin(self, arr, n):
+        #complete the function here
+        return min(arr)
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = [int(x) for x in input().split()]
+        ob = Solution()
+        print(ob.findMin(arr, n))
+# } Driver Code Ends
