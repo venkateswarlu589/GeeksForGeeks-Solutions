ad4c0fb10d940470d376a4a6acfa54c976cb58a3
Venkateswarlu Kasireddy <85172370+venkateswarlu589@users.noreply.github.com>
2023-11-29 15:41:23 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/First Repeating Element/first-repeating-element.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    #Function to return the position of the first repeating element.
+    def firstRepeated(self,arr, n):
+        
+        #arr : given array
+        #n : size of the array
+        position = n
+        d = {}
+        for i, elem in enumerate(arr):
+            if elem in d and d[elem] < position:
+                position = d[elem]
+            d[elem] = i
+        return position + 1 if position < n else -1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        
+        arr=[int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.firstRepeated(arr, n))
+# } Driver Code Ends
